3b84a62a6cc6fa0506dd5b9d18b9e37f3ba5aaa8
Jon Nordby <jononor@gmail.com>
2016-02-19 20:30:15 +0100

    Split into different functions

--- src/bin/msgflo-rust.rs
+++ src/bin/msgflo-rust.rs
@@ -5,38 +5,47 @@ use amqp::{ConsumerCallBackFn, Session, Table, Basic, Channel, Options};
 use amqp::protocol;
 use std::default::Default;
 
+// for debugging
+fn listen_discovery(channel: &mut Channel) {
+    fn participant_discovered(channel: &mut Channel, deliver: protocol::basic::Deliver,
+                            headers: protocol::basic::BasicProperties, body: Vec<u8>){
+        println!("Discovered MsgFlo participant !!:");
+        //println!("Deliver info: {:?}", deliver);
+        //println!("Content headers: {:?}", headers);
+        //println!("Content body: {:?}", body);
+        channel.basic_ack(deliver.delivery_tag, false);
+    }
 
-fn consumer_function(channel: &mut Channel, deliver: protocol::basic::Deliver, headers: protocol::basic::BasicProperties, body: Vec<u8>){
-    println!("Got a delivery:");
-    println!("Deliver info: {:?}", deliver);
-    println!("Content headers: {:?}", headers);
-    println!("Content body: {:?}", body);
-    channel.basic_ack(deliver.delivery_tag, false);
+    let queue_name = "fbp";
+    let consumer_name = channel.basic_consume(participant_discovered as ConsumerCallBackFn, queue_name,
+                                                "", false, false, false, false, Table::new());
 }
 
-fn main() {
-    let mut session = Session::new(Options{vhost: "/", .. Default::default()}).ok().expect("Can't create session");
-    println!("Openned session:"); 
-    let mut channel = session.open_channel(1).expect("channel");
-    println!("Openned channel: {}", channel.id);
+fn send_discovery(channel: &mut Channel) {
+    let queue_name = "fbp"; // TODO: use an exchange istead, requires protocol change in msgflo
 
-    let queue_name = "test_queue";
-    //queue: &str, passive: bool, durable: bool, exclusive: bool, auto_delete: bool, nowait: bool, arguments: Table
     let queue_declare = channel.queue_declare(queue_name, false, true, false, false, false, Table::new());
-    println!("Queue declare: {:?}", queue_declare);
+    let content_type = Some("application/json".to_string());
+    let props = protocol::basic::BasicProperties { content_type: content_type, ..Default::default() };
 
-    //queue: &str, consumer_tag: &str, no_local: bool, no_ack: bool, exclusive: bool, nowait: bool, arguments: Table
-    println!("Declaring consumer...");
-    let consumer_name = channel.basic_consume(consumer_function as ConsumerCallBackFn, queue_name, "", false, false, false, false, Table::new());
-    println!("Starting consumer {:?}", consumer_name);
+    let payload = b"Hello from rust!";
+    let res = channel.basic_publish("", queue_name, true, false, props, payload.to_vec());
+}
 
-    let props = protocol::basic::BasicProperties{ content_type: Some("text".to_string()), ..Default::default()};
-    let res = channel.basic_publish("", queue_name, true, false, props, (b"Hello from rust!").to_vec());
- 
-    println!("Published");
+// for debugging
+fn selftest() {
+    let mut session = Session::new(Options{vhost: "/", .. Default::default()}).ok().expect("Can't create session");
+    let mut channel = session.open_channel(1).expect("channel");
+
+    send_discovery(&mut channel);
+    listen_discovery(&mut channel);
 
     channel.start_consuming();
 
     let closed = channel.close(200, "Bye".to_string());
     session.close(200, "Good Bye".to_string());
+}
+
+fn main() {
+    selftest();
 }
