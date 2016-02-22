ab896d7f7f30ed21f7218125907fe3fd8419f9d7
Jon Nordby <jononor@gmail.com>
2016-02-22 03:06:51 +0100

    examples: Formatting

--- examples/repeat.rs
+++ examples/repeat.rs
@@ -1,27 +1,22 @@
 extern crate msgflo;
-
 use msgflo::participant::{Participant, InfoBuilder, Info};
 
 struct Repeat {
     state: Option<String>, // we don't really have any state
 }
-
 impl Participant for Repeat {
     fn info(&self) -> Info {
-        InfoBuilder::new("rust/Repeat")
-            .label("Repeats input as-is")
+        InfoBuilder::new("rust/Repeat").label("Repeats input as-is")
             .inport("in")
             .outport("out")
             .build()
     }
-
+    // Will be called with input AMQP data on 'in' port, and send to 'out'
     fn process(&self, input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
         println!("repeat process():");
         return Ok(input);
     }
-
 }
-
 fn main() {
     static r: Repeat = Repeat { state: None };
     msgflo::participant::main(&r);
